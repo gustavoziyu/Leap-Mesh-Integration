b24c2783506161d2d6e7bd2f5b314684f890cb0f
guilhermekobori <koboriguilherme@gmail.com>
2019-10-18 11:13:07 -0300

    Fixed bugs in debug prefab and started testing rotation and translation, a bug persists in rotatios

--- Assets/Scripts/OculusTouchDebug.cs
+++ Assets/Scripts/OculusTouchDebug.cs
@@ -32,21 +32,21 @@ public class OculusTouchDebug : MonoBehaviour
         if (OVRInput.Get(OVRInput.Button.Three)) buttonThreeStatus = 2;
         else if (OVRInput.Get(OVRInput.Touch.Three)) buttonThreeStatus = 1;
         else buttonThreeStatus = 0;
-        setButtonStatusMessage(buttonThreeStatusText, "Button Three (B): ", buttonThreeStatus);
+        setButtonStatusMessage(buttonThreeStatusText, "Button Three (X): ", buttonThreeStatus);
         
         //Button four (Y) tracking
         if (OVRInput.Get(OVRInput.Button.Four)) buttonFourStatus = 2;
         else if (OVRInput.Get(OVRInput.Touch.Four)) buttonFourStatus = 1;
         else buttonFourStatus = 0;
-        setButtonStatusMessage(buttonFourStatusText, "Button Four (B): ", buttonFourStatus);
+        setButtonStatusMessage(buttonFourStatusText, "Button Four (Y): ", buttonFourStatus);
 
         //Left index trigger tracking
-        leftIndexTrigger = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+        leftIndexTrigger = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
         leftIndexTriggerText.text = "Left index trigger position: " + leftIndexTrigger;
         Debug.Log("Left index trigger position: " + leftIndexTrigger);
        
         //Left hand trigger tracking
-        leftHandTrigger = OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger);
+        leftHandTrigger = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger);
         leftHandTriggerText.text = "Left hand trigger position: " + leftHandTrigger;
         Debug.Log("Left hand trigger position: " + leftHandTrigger);
 
@@ -54,6 +54,7 @@ public class OculusTouchDebug : MonoBehaviour
         leftThumbstickPosition = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
         leftThumbstickXAxisText.text = "Left thumbstick X position: " + leftThumbstickPosition.x;
         leftThumbstickYAxisText.text = "Left thumbstick Y position: " + leftThumbstickPosition.y;
+
     }
 
     void setButtonStatusMessage(Text text, string tag, int status) {
@@ -62,6 +63,7 @@ public class OculusTouchDebug : MonoBehaviour
         else if (status == 1) statusMessage = "Touched";
         else if (status == 2) statusMessage = "Pressed";
         else statusMessage = "UNKNOWN STATUS VALUE";
+        Debug.Log(tag + leftIndexTrigger);
 
         text.text = tag + statusMessage;
     }

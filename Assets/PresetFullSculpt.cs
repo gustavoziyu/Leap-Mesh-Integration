2f9362a7c3909f409041437a205b63ca68ed7465
guilhermekobori <koboriguilherme@gmail.com>
2019-12-02 16:56:16 -0300

    sculpt vr

--- Assets/PresetFullSculpt.cs
+++ Assets/PresetFullSculpt.cs
@@ -2,27 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PresetFullSculpt : MonoBehaviour
+namespace Leap.Unity
 {
-    void Awale()
+    public class PresetFullSculpt : MonoBehaviour
     {
-        GameObject target = GameObject.FindWithTag("ModelObject");
-        target.GetComponent<MeshCollider>().isTrigger = false;
-        target.GetComponent<MeshCollider>().convex = false;
-        target.transform.position = new Vector3(-2.867f, 0, 0);
-        target.transform.rotation = new Quaternion(0, 0, 0, 0);
-        if (target.name == "Revolution" || target.name == "Extrusion")
-            target.transform.localScale = new Vector3(10, 10, 10);
-        else
-            target.transform.localScale = new Vector3(2, 2, 2);
-        GetComponent<Leap.Unity.RotateModelTouch>().target = target;
-        GetComponent<Leap.Unity.ResizeModelTouch>().target = target;
+        private void Awake()
+        {
+            GameObject target = GameObject.FindWithTag("ModelObject");
+            if (target == null)
+                Debug.Log("Model object not found! Make sure you have a object with the tag ModelObject on the scene.");
+            else
+            {
+                GetComponent<RotateModelTouch>().target = target;
+                GetComponent<ResizeModelTouch>().target = target;
+                target.GetComponent<MeshCollider>().isTrigger = false;
+                target.GetComponent<MeshCollider>().convex = false;
+                target.transform.position = new Vector3(-3, 0, 0);
+                target.transform.rotation = new Quaternion(0, 0, 0, 0);
+                if (target.name == "Revolution" || target.name == "Extrusion") ;
+                //target.transform.localScale = new Vector3(10, 10, 10);
+                else
+                    target.transform.localScale = new Vector3(1, 1, 1);
+            }
+            GameObject goal = GameObject.FindWithTag("GoalModel");
+            if (goal != null)
+            {
+                goal.GetComponent<MeshCollider>().isTrigger = false;
+                goal.GetComponent<MeshCollider>().convex = false;
+                goal.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+                goal.transform.position = GameObject.Find("GoalModelPosition").transform.position;
+                goal.transform.rotation = new Quaternion(0, 0, 0, 0);
+                goal.AddComponent<RotateModelDisplay>();
+            }
+            else
+            {
+                GameObject.Find("Spot Light").SetActive(false);
+                GameObject.Find("Spot Light 2").SetActive(false);
+            }
 
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void placeObjectInCorner(GameObject myObj)
+        {
+            Camera camera = GameObject.Find("Camera").GetComponent<Camera>();
+            Vector3 sizeOfObject = myObj.GetComponent<Renderer>().bounds.size;
+            Vector3 upperLeft = camera.ViewportToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
+            upperLeft.x = upperLeft.x + sizeOfObject.x / 2;
+            upperLeft.y = upperLeft.y + sizeOfObject.y / 2;
+            upperLeft.z = upperLeft.z + sizeOfObject.z / 2;
+
+            myObj.transform.position = upperLeft;
+
+        }
     }
 }
